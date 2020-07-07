f661a223bc3034a68ddb8a8120863c16b50cf0fb
andrassziffer <andrassziffer@gmail.com>
2020-07-07 14:28:29 +0200

    Initial Commit

---
+++ Challenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Challenger
+//
+//  Created by Andras Sziffer on 2020. 07. 07..
+//  Copyright © 2020. Andras Sziffer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
